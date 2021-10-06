e215760b84b4d00906612d7f31e0de90268e1cb8
Kevin Kibet <kevinkibetw7@gmail.com>
2021-10-06 09:16:37 +0300

    Initial Commit

---
+++ Segues&Navs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Segues&Navs
+//
+//  Created by Kevin Lagat on 06/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
